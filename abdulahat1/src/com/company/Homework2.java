1dc445c735fcfc0150eb4ca832a23708145d46ab
abdulahat1 <2267951086@qq.com>
2021-09-26 11:28:43 +0800

    homwork

--- abdulahat1/src/com/company/Homework2.java
+++ abdulahat1/src/com/company/Homework2.java
@@ -8,8 +8,9 @@ public class Homework2 {
     public class SumTest {
         @Test
         public void test() {
-
-            int nums[] = {1, -2, 3, -2, 5, 1}; // 测试用例1 最大值7
+            /* int nums[] = {1,-2,3,5,-1}; */// 测试用例1 最大值8
+            /* int nums[] = {1,-2,3,-8,5,1}; */// 测试用例1 最大值6
+            int nums[] = { 1, -2, 3, -2, 5, 1 }; // 测试用例1 最大值7
             Sum sum = new Sum();
             sum.sumParams(nums);
         }
